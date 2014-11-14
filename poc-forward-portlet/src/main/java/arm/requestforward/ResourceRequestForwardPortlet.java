703d60a96032cc485534ad20c64538ef33238e05
ar <a.rodriguez@comundus.com>
2014-11-14 12:21:34 +0100

    Use redirection and session attributes

--- poc-forward-portlet/src/main/java/arm/requestforward/ResourceRequestForwardPortlet.java
+++ poc-forward-portlet/src/main/java/arm/requestforward/ResourceRequestForwardPortlet.java
@@ -6,16 +6,21 @@ import java.io.InputStream;
 
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
-import javax.portlet.PortletRequest;
 import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.util.PortalUtil;
 
 /**
  * Portlet implementation class ResourceRequestForwardPortlet
@@ -24,15 +29,17 @@ public class ResourceRequestForwardPortlet extends GenericPortlet {
 
     public void init() {
         viewTemplate = getInitParameter("view-template");
-        errorTemplate = getInitParameter("error-template");
     }
 
     public void serveResource(
             ResourceRequest request, ResourceResponse response)
         throws PortletException, IOException {
 
+        
         String action = request.getParameter("action");
         if ("success".equals(action)) {
+            
+            //Everything should work and the image will be shown
             response.setContentType("image/png");
             
             InputStream is =ResourceRequestForwardPortlet.class.getResourceAsStream("/liferay-logo.png");
@@ -40,35 +47,58 @@ public class ResourceRequestForwardPortlet extends GenericPortlet {
             byte[] logoBytes = inputStream2Bytes(is); 
             response.getPortletOutputStream().write(logoBytes);
             response.flushBuffer();
+        } else if ("redirect".equals(action)) {
+
+            //Suppose the image generation went wrong, and we do a redirection
+            redirect ("Action: redirect", request, response);
         } else {
+
+            // Suppose the image generation went wrong, and we try to do a forward which should work
+            // like the redirection but without using the session to store "request"-scoped attributes
             request.setAttribute("error", "Unexpected action: " + action);
-            forward(errorTemplate, request, response);
+            forwardResourceRequest("Unexpected action: " + action, request, response);
         }
     }
 
-    public void doView(
-            RenderRequest renderRequest, RenderResponse renderResponse)
+    public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
         throws IOException, PortletException {
 
+        String error = (String)SessionErrors.get(renderRequest, "resourcerequest.error");
+        SessionErrors.clear(renderRequest);
+        
+        String message = (String)SessionMessages.get(renderRequest, "resourcerequest.message");
+        SessionMessages.clear(renderRequest);
+        
+        renderRequest.setAttribute("errorMsg", error);
+        renderRequest.setAttribute("infoMsg", message);
         include(viewTemplate, renderRequest, renderResponse);
     }
 
-    protected void forward(String path, PortletRequest renderRequest,
-            PortletResponse renderResponse)
+    protected void forwardResourceRequest(String errorText, ResourceRequest portletRequest, ResourceResponse portletResponse)
         throws IOException, PortletException {
 
-        PortletRequestDispatcher portletRequestDispatcher =
-            getPortletContext().getRequestDispatcher(path);
 
-        if (portletRequestDispatcher == null) {
-            _log.error(path + " is not a valid forward");
-        }
-        else {
-            portletRequestDispatcher.forward(renderRequest, renderResponse);
+        HttpServletRequest request = PortalUtil.getOriginalServletRequest(PortalUtil.getHttpServletRequest(portletRequest));
+        
+        PortletURL renderURL = portletResponse.createRenderURL();
+        
+        try {
+            request.setAttribute("error", errorText);            
+            RequestDispatcher rd = request.getRequestDispatcher(renderURL.toString().replace("http://localhost:8080", ""));
+            rd.forward(request, PortalUtil.getHttpServletResponse(portletResponse));
+        } catch (ServletException e) {
+            throw new PortletException(e);
         }
     }
-
     
+    protected void redirect (String errorText, ResourceRequest resourceRequest,
+            ResourceResponse resourceResponse) {
+        
+        SessionErrors.add(resourceRequest, "resourcerequest.error", "ERROR " + errorText);
+        SessionMessages.add(resourceRequest, "resourcerequest.message", "MESSAGE " + errorText);
+        resourceResponse.setProperty(ResourceResponse.HTTP_STATUS_CODE, "302");
+        resourceResponse.addProperty("Location", resourceResponse.createRenderURL().toString());
+    }
     
     protected void include(
             String path, RenderRequest renderRequest,
@@ -103,7 +133,6 @@ public class ResourceRequestForwardPortlet extends GenericPortlet {
     }
     
     protected String viewTemplate;
-    protected String errorTemplate;
 
     private static Log _log = LogFactoryUtil.getLog(ResourceRequestForwardPortlet.class);
 
--- poc-forward-portlet/src/main/webapp/html/resourcerequestforward/view.jsp
+++ poc-forward-portlet/src/main/webapp/html/resourcerequestforward/view.jsp
@@ -10,11 +10,13 @@ successURL.setParameter("action", "success");
 ResourceURL failURL = renderResponse.createResourceURL();
 failURL.setParameter("action", "fail");
 
+ResourceURL redirectURL = renderResponse.createResourceURL();
+redirectURL.setParameter("action", "redirect");
+
 %>
 This is the <b>Resource Request Forward</b> portlet in View mode.
 
-<p>You can provoke the next call to <a href="<%=failURL%>">fail</a> or to succeed by <a href="<%=successURL%>">downloading a picture</a>.
+<p>You can provoke the next call to <a href="<%=failURL%>">fail</a>, to succeed by <a href="<%=successURL%>">downloading a picture</a> or just make a <a href="<%=redirectURL%>">redirection</a>.
 
-<p>ERROR: ${error}
-
-
+<p>Error message: ${errorMsg}
+<p>Info message: ${infoMsg}
